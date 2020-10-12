6c05ff4eca74b5e82c7b4de4444a794cafcaca0c
Ayushish93 <ayushi08sharma@gmail.com>
2020-10-12 22:56:18 +0000

    BootcampX Queries-2 Join completed

--- 1_queries/6_graduates_without_github.sql
+++ 1_queries/6_graduates_without_github.sql
@@ -2,3 +2,10 @@ SELECT name, email, phone
 FROM students
 WHERE github IS NULL
 AND end_date IS NOT NULL;
+
+
+-- querie for rollover students
+
+--  SELECT students.name AS student_name, cohorts.name AS cohort_name, students.start_date AS student_start_date, cohorts.start_date AS cohorts_start_date
+--  FROM students JOIN cohorts ON students.cohort_id = cohorts.id 
+-- WHERE students.start_date != cohorts.start_date ORDER BY cohorts.start_date;
